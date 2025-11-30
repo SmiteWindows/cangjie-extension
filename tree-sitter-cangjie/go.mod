823d9686f48c1bbac5b9f23aca8e11a03eff987a
Smite <1627283564@qq.com>
2025-11-30 09:54:28 +0800

    将tree-sitter-cangjie添加为子模块，解决git冲突

--- tree-sitter-cangjie/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/smitewindows/cangjie-extension
-
-go 1.22
-
-require github.com/tree-sitter/go-tree-sitter v0.24.0
--- tree-sitter-cangjie/run-ps-script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Batch file to ensure PowerShell 7 is used for executing scripts
-
-REM Execute the script with PowerShell 7
-pwsh -ExecutionPolicy Bypass -File %1 %2 %3 %4 %5 %6 %7 %8 %9
